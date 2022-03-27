54a23555fe74f4cd388283e3181123dbfbfe32be
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-03-27 10:51:51 -0400

    Async not working

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# The case for async
+# Todo
+That's just scratching the surface though. Those are events, but we can also have spans: we could have a span for the whole server, another for accepting connections, another for our connection handler, one for accepting incoming connections, one for reading the incoming request and one for writing the response:
 `https://fasterthanli.me/articles/request-coalescing-in-async-rust`
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,57 @@
 use std::{error::Error, net::SocketAddr};
 
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpListener,
+    io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
 };
+
+use tracing::{debug, info};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let rt = tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()?;
+    tracing_subscriber::fmt().init();
+    run_server().await
+}
+
+#[tracing::instrument]
+async fn run_server() -> Result<(), Box<dyn Error>> {
     let addr: SocketAddr = "0.0.0.0:3799".parse()?;
-    println!("Listening on http://{}", addr);
+    info!("Listening on http://{}", addr);
     let listener = TcpListener::bind(addr).await?;
     loop {
         let (mut stream, addr) = listener.accept().await?;
-        println!("Accepted connection from {addr}");
-
-        let mut incoming = vec![];
-        loop {
-            let mut buf = vec![0u8; 1024];
-            let read = stream.read(&mut buf).await?;
-            incoming.extend_from_slice(&buf[..read]);
-
-            if incoming.len() > 4 && &incoming[incoming.len() - 4..] == b"\r\n\r\n" {
-                break;
-            }
-        }
-
-        let incoming = std::str::from_utf8(&&incoming)?;
-        println!("Got HTTP request:\n{}", incoming);
-        stream.write_all(b"HTTP/1.1 200 Ok\r\n").await?;
-        stream.write_all(b"\r\n").await?;
-        stream.write_all(b"Hello from plaque!\n").await?;
-        println!("Closing connection from {addr}");
+        handle_connection(stream, addr).await?;
     }
+}
+
+#[tracing::instrument(skip(stream))]
+async fn handle_connection(mut stream: TcpStream, addr: SocketAddr) -> Result<(), Box<dyn Error>> {
+    let req = read_http_request(&mut stream).await?;
+    debug!(%req,"Got HTTP request");
+    write_http_response(&mut stream).await?;
+    Ok(())
+}
+
+#[tracing::instrument(skip(stream))]
+async fn read_http_request(mut stream: impl AsyncRead + Unpin) -> Result<String, Box<dyn Error>> {
+    let mut incoming = vec![];
+
+    loop {
+        let mut buf = vec![0u8; 1024];
+        let read = stream.read(&mut buf).await?;
+        incoming.extend_from_slice(&buf[..read]);
+
+        if incoming.len() > 4 && &incoming[incoming.len() - 4..] == b"\r\n\r\n" {
+            break;
+        }
+    }
+    Ok(String::from_utf8(incoming)?)
+}
+
+#[tracing::instrument(skip(stream))]
+async fn write_http_response(mut stream: impl AsyncWrite + Unpin) -> Result<(), Box<dyn Error>> {
+    stream.write_all(b"HTTP/1.1 200 Ok\r\n").await?;
+    stream.write_all(b"\r\n").await?;
+    stream.write_all(b"Hello from plaque!\n").await?;
+    Ok(())
 }
