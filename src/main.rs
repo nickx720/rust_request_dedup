0144104fd1d4c5be63ad2aec54e245719067e972
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-03-27 09:01:21 -0400

    Threaded Solution

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     error::Error,
     io::{Read, Write},
-    net::{SocketAddr, TcpListener},
+    net::{SocketAddr, TcpListener, TcpStream},
 };
 fn main() -> Result<(), Box<dyn Error>> {
     let addr: SocketAddr = "0.0.0.0:3799".parse()?;
@@ -10,24 +10,33 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     loop {
         let (mut stream, addr) = listener.accept()?;
-        println!("Accepted connection from {addr}");
-
-        let mut incoming = vec![];
-
-        loop {
-            let mut buf = vec![0u8; 1024];
-            let read = stream.read(&mut buf)?;
-            incoming.extend_from_slice(&buf[..read]);
-
-            if incoming.len() > 4 && &incoming[incoming.len() - 4..] == b"\r\n\r\n" {
-                break;
+        std::thread::spawn(move || {
+            if let Err(e) = handle_connection(stream, addr) {
+                println!("Error handling connection {addr}: {e}");
             }
-        }
-        let incoming = std::str::from_utf8(&&incoming)?;
-        println!("Got HTTP request:\n{}", incoming);
-        stream.write_all(b"HTTP/1.1 200 OK\r\n")?;
-        stream.write_all(b"\r\n")?;
-        stream.write_all(b"Hello from plaque!\n")?;
-        println!("Closing connection for {addr}");
+        });
     }
+}
+
+fn handle_connection(mut stream: TcpStream, addr: SocketAddr) -> Result<(), Box<dyn Error>> {
+    println!("Accepted connection from {addr}");
+
+    let mut incoming = vec![];
+
+    loop {
+        let mut buf = vec![0u8; 1024];
+        let read = stream.read(&mut buf)?;
+        incoming.extend_from_slice(&buf[..read]);
+
+        if incoming.len() > 4 && &incoming[incoming.len() - 4..] == b"\r\n\r\n" {
+            break;
+        }
+    }
+    let incoming = std::str::from_utf8(&&incoming)?;
+    println!("Got HTTP request:\n{}", incoming);
+    stream.write_all(b"HTTP/1.1 200 OK\r\n")?;
+    stream.write_all(b"\r\n")?;
+    stream.write_all(b"Hello from plaque!\n")?;
+    println!("Closing connection for {addr}");
+    Ok(())
 }
