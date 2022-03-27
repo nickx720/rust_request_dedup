aae8464136ecaff0ecd2485b562f6101fef7cc74
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-03-27 10:27:17 -0400

    Adding tracing

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,38 @@
-use std::{
-    error::Error,
-    io::{Read, Write},
-    net::{SocketAddr, TcpListener, TcpStream},
+use std::{error::Error, net::SocketAddr};
+
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
 };
-fn main() -> Result<(), Box<dyn Error>> {
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()?;
     let addr: SocketAddr = "0.0.0.0:3799".parse()?;
-    let listener = TcpListener::bind(addr)?;
     println!("Listening on http://{}", addr);
-
+    let listener = TcpListener::bind(addr).await?;
     loop {
-        let (mut stream, addr) = listener.accept()?;
-        std::thread::spawn(move || {
-            if let Err(e) = handle_connection(stream, addr) {
-                println!("Error handling connection {addr}: {e}");
+        let (mut stream, addr) = listener.accept().await?;
+        println!("Accepted connection from {addr}");
+
+        let mut incoming = vec![];
+        loop {
+            let mut buf = vec![0u8; 1024];
+            let read = stream.read(&mut buf).await?;
+            incoming.extend_from_slice(&buf[..read]);
+
+            if incoming.len() > 4 && &incoming[incoming.len() - 4..] == b"\r\n\r\n" {
+                break;
             }
-        });
-    }
-}
-
-fn handle_connection(mut stream: TcpStream, addr: SocketAddr) -> Result<(), Box<dyn Error>> {
-    println!("Accepted connection from {addr}");
-
-    let mut incoming = vec![];
-
-    loop {
-        let mut buf = vec![0u8; 1024];
-        let read = stream.read(&mut buf)?;
-        incoming.extend_from_slice(&buf[..read]);
-
-        if incoming.len() > 4 && &incoming[incoming.len() - 4..] == b"\r\n\r\n" {
-            break;
         }
+
+        let incoming = std::str::from_utf8(&&incoming)?;
+        println!("Got HTTP request:\n{}", incoming);
+        stream.write_all(b"HTTP/1.1 200 Ok\r\n").await?;
+        stream.write_all(b"\r\n").await?;
+        stream.write_all(b"Hello from plaque!\n").await?;
+        println!("Closing connection from {addr}");
     }
-    let incoming = std::str::from_utf8(&&incoming)?;
-    println!("Got HTTP request:\n{}", incoming);
-    stream.write_all(b"HTTP/1.1 200 OK\r\n")?;
-    stream.write_all(b"\r\n")?;
-    stream.write_all(b"Hello from plaque!\n")?;
-    println!("Closing connection for {addr}");
-    Ok(())
 }
---
+++ src/non_tokio.rs
@@ -0,0 +1,42 @@
+use std::{
+    error::Error,
+    io::{Read, Write},
+    net::{SocketAddr, TcpListener, TcpStream},
+};
+fn main() -> Result<(), Box<dyn Error>> {
+    let addr: SocketAddr = "0.0.0.0:3799".parse()?;
+    let listener = TcpListener::bind(addr)?;
+    println!("Listening on http://{}", addr);
+
+    loop {
+        let (mut stream, addr) = listener.accept()?;
+        std::thread::spawn(move || {
+            if let Err(e) = handle_connection(stream, addr) {
+                println!("Error handling connection {addr}: {e}");
+            }
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream, addr: SocketAddr) -> Result<(), Box<dyn Error>> {
+    println!("Accepted connection from {addr}");
+
+    let mut incoming = vec![];
+
+    loop {
+        let mut buf = vec![0u8; 1024];
+        let read = stream.read(&mut buf)?;
+        incoming.extend_from_slice(&buf[..read]);
+
+        if incoming.len() > 4 && &incoming[incoming.len() - 4..] == b"\r\n\r\n" {
+            break;
+        }
+    }
+    let incoming = std::str::from_utf8(&&incoming)?;
+    println!("Got HTTP request:\n{}", incoming);
+    stream.write_all(b"HTTP/1.1 200 OK\r\n")?;
+    stream.write_all(b"\r\n")?;
+    stream.write_all(b"Hello from plaque!\n")?;
+    println!("Closing connection for {addr}");
+    Ok(())
+}
